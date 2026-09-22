51734e788ff262e882c2604d167b086d23522714
Birkegard <birkegard@hotmail.se>
2026-09-22 10:24:01 +0200

    feat: add Header component with navigation links

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router";
+
+const Header = () => {
+  return (
+    <header className="text-center mt-3">
+      <h1 className="text-zinc-800 font-bold text-6xl p-2">Group 2 TechShop</h1>
+      <nav className="flex gap-2 justify-center">
+        <NavLink
+          to="/"
+          className="p-1 font-semibold text-blue-950 hover:text-blue-900 hover:underline active:text-blue-700"
+        >
+          Hem
+        </NavLink>
+        <NavLink
+          to="/products"
+          className="p-1 font-semibold text-blue-950 hover:text-blue-900 hover:underline active:text-blue-700"
+        >
+          Produkter
+        </NavLink>
+        <NavLink
+          to="/login"
+          className="p-1 font-semibold text-blue-950 hover:text-blue-900 hover:underline active:text-blue-700"
+        >
+          Logga in
+        </NavLink>
+        <NavLink
+          to="/register"
+          className="p-1 font-semibold text-blue-950 hover:text-blue-900 hover:underline active:text-blue-700"
+        >
+          Registrera
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
