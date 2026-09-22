4b69c8acacf93660d08cc86d80d9bad923566ef2
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-09-22 15:19:30 +0200

    feat: update API base URL to use environment variable for product fetching

--- src/service/ProductService.ts
+++ src/service/ProductService.ts
@@ -1,6 +1,6 @@
 import type {ProductResponse} from "../types/ProductResponse.ts";
 
-const API_BASE_URL = "http://localhost:5002/api/products";
+const API_BASE_URL = import.meta.env.VITE_PRODUCT_API_URL || "http://localhost:5002/api/products";
 
 export async function fetchAllProducts(): Promise<ProductResponse[]> {
     const response = await fetch(API_BASE_URL);
